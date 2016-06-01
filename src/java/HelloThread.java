1aea7236074a2aa32063685bbeff8a877618e8b4
Olga Agafonova <volga.ahafonava@gmail.com>
2016-06-01 10:24:11 +0300

    Created two classes, which used two ways to create an instance of Thread

---
+++ src/java/HelloThread.java
@@ -0,0 +1,12 @@
+package java;
+
+public class HelloThread extends Thread {
+
+    public void run() {
+        System.out.println("Hello from a thread!");
+    }
+
+    public static void main(String args[]) {
+        (new HelloThread()).start();
+    }
+}
