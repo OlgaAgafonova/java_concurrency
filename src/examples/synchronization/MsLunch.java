15c96f581c94b3db1e516aa950ced81a41b73347
Olga Agafonova <volga.ahafonava@gmail.com>
2016-06-01 14:22:31 +0300

    created class MsLunch

--- src/examples/synchronization/MsLunch.java
+++ src/examples/synchronization/MsLunch.java
@@ -6,14 +6,14 @@ public class MsLunch {
     private Object lock1 = new Object();
     private Object lock2 = new Object();
 
-    public void inc1(){
-        synchronized (lock1){
+    public void inc1() {
+        synchronized (lock1) {
             c1++;
         }
     }
 
-    public void inc2(){
-        synchronized (lock2){
+    public void inc2() {
+        synchronized (lock2) {
             c2++;
         }
     }
